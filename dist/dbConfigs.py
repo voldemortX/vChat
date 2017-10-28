051702d180294b1c5b1a79cfd4f266c1b26fa4fe
voldemort <32259501+voldemortX@users.noreply.github.com>
2017-10-28 14:48:08 +0800

    Update dbConfigs.py

--- dist/dbConfigs.py
+++ dist/dbConfigs.py
@@ -4,7 +4,7 @@ import pymysql
 vChatConfigs = {'host':'127.0.0.1',
                 'port':3306,
                 'user':'root',
-                'password':"fshm0113",
+                'password':"********",
                 'db':"vchat",
                 'charset':'utf8',
                 'cursorclass': pymysql.cursors.DictCursor};
